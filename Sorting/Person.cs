5d3679f8b1b1f66f4407450ce64b48be95644653
Justin Obney <justinobney@gmail.com>
2012-12-11 18:40:06 -0600

    Added Sorting Project for Demonstration

---
+++ Sorting/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public class Person: IComparable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public int CompareTo(Object obj)
+        {
+            var otherPerson = (Person) obj;
+            return String.Compare(this.LastName, otherPerson.LastName);
+        }
+
+        public static int Compare(Person person1, Person person2)
+        {
+            return person1.CompareTo(person2);
+        }
+    }
+}
---
+++ Sorting/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person1 = new Person()
+            {
+                LastName = "Smith",
+                FirstName = "John",
+                Age = 36
+            };
+
+            var person2 = new Person()
+            {
+                LastName = "Jones",
+                FirstName = "John",
+                Age = 46
+            };
+
+            var person3 = new Person()
+            {
+                LastName = "Smith",
+                FirstName = "John",
+                Age = 26
+            };
+
+            Console.WriteLine(Person.Compare(person1, person3));
+            Console.ReadLine();
+        }
+    }
+}
