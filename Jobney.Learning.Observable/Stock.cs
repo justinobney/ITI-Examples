6e8e06bb5df54130a611b27e25438b1e181ff703
Justin Obney <justin@envoc.com>
2013-05-21 07:11:11 -0500

    Observable code sample

---
+++ Jobney.Learning.Observable/Stock.cs
@@ -0,0 +1,30 @@
+﻿namespace Jobney.Learning.Observable
+{
+    public class Stock
+    {
+
+        //declare a delegate for the event
+        public delegate void AskPriceDelegate(object aPrice);
+        //declare the event using the delegate
+        public event AskPriceDelegate AskPriceChanged;
+
+        //instance variable for ask price
+        object askPrice;
+
+        //property for ask price
+        public object AskPrice
+        {
+
+            set
+            {
+                //set the instance variable
+                askPrice = value;
+
+                //fire the event
+                AskPriceChanged(askPrice);
+            }
+
+        }//AskPrice property
+
+    }//Stock class
+}
