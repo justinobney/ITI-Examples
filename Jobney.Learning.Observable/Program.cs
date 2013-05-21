f0d89f1d87df713db9953d0054414c08219a9e1b
Justin Obney <justin@envoc.com>
2013-05-21 07:11:11 -0500

    Observable code sample

---
+++ Jobney.Learning.Observable/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Jobney.Learning.Observable
+{
+    public static class MainClass
+    {
+        public static void Main()
+        {
+
+            //create new display and stock instances
+            var stockDisplay = new StockDisplay();
+            var stock = new Stock();
+
+            //create a new delegate instance and bind it
+            //to the observer's askpricechanged method
+            var aDelegate = new
+               Stock.AskPriceDelegate(stockDisplay.AskPriceChanged);
+
+            //add the delegate to the event
+            stock.AskPriceChanged += aDelegate;
+
+            //loop 100 times and modify the ask price
+            for (var looper = 0; looper < 100; looper++)
+            {
+                stock.AskPrice = looper;
+            }
+
+            //remove the delegate from the event
+            stock.AskPriceChanged -= aDelegate;
+
+            Console.WriteLine("Complete..");
+            Console.ReadLine();
+
+        }//Main
+
+    }//MainClass
+}
---
+++ Jobney.Learning.Observable/StockDisplay.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Jobney.Learning.Observable
+{
+    //represents the user interface in the application
+    public class StockDisplay
+    {
+
+        public void AskPriceChanged(object aPrice)
+        {
+            Console.Write("The new ask price is:" + aPrice + "\r\n");
+        }
+
+    }//StockDispslay class
+}
