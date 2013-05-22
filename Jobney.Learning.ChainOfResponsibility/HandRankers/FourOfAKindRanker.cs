95290ab8d5b31838403fef779662724efa89081c
Justin Obney <justin@envoc.com>
2013-05-22 08:05:18 -0500

    Coded Four of a kind ranker

---
+++ Jobney.Learning.ChainOfResponsibility/HandRankers/FourOfAKindRanker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Jobney.Learning.ChainOfResponsibility.Enums;
+
+namespace Jobney.Learning.ChainOfResponsibility.HandRankers
+{
+    public class FourOfAKindRanker : HandRanker
+    {
+        public FourOfAKindRanker(HandRanker nextRanker)
+            : base(nextRanker)
+        {
+
+        }
+
+        public override HandRanking Handle(IEnumerable<Card> hand)
+        {
+            if (hand.GroupBy(x => x.Value.Value).Any(grp => grp.Count() == 4))
+            {
+                CanHandle = true;
+                return HandRanking.FourOfAKind;
+            }
+            return next.Handle(hand);
+        }
+    }
+}
---
+++ Jobney.Learning.ChainOfResponsibility/Tests/FourOfAKindRankerTest.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using Jobney.Learning.ChainOfResponsibility.Enums;
+using Jobney.Learning.ChainOfResponsibility.HandRankers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jobney.Learning.ChainOfResponsibility.Tests
+{
+    [TestClass]
+    public class FourOfAKindRankerTest
+    {
+        public FourOfAKindRanker target;
+        public HighCardRanker highcardRanker;
+        private IEnumerable<Card> hand;
+
+        [TestInitialize]
+        public void Init()
+        {
+            highcardRanker = new HighCardRanker();
+            target = new FourOfAKindRanker(highcardRanker);
+        }
+
+        [TestClass]
+        public class TheHandleMethod : FourOfAKindRankerTest
+        {
+            [TestMethod]
+            public void ReturnsFourOfAKindRankWhenHandContainsFourOfAKind()
+            {
+                // Arrange
+                var expected = HandRanking.FourOfAKind;
+                hand = new List<Card>()
+                    {
+                        new Card(){Suit = CardSuit.Clubs, Value = CardValue.Ace},
+                        new Card(){Suit = CardSuit.Hearts, Value = CardValue.Ace},
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.Ace},
+                        new Card(){Suit = CardSuit.Diamonds, Value = CardValue.Ace},
+                        new Card(){Suit = CardSuit.Clubs, Value = CardValue.Jack}
+                    };
+
+                // Act
+                var actual = target.Handle(hand);
+
+                // Assert
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestMethod]
+            public void ReturnsHighCardRankWhenNoFourOfAKindExists()
+            {
+                // Arrange
+                var expected = HandRanking.HighCard;
+                hand = new List<Card>()
+                    {
+                        new Card(){Suit = CardSuit.Clubs, Value = CardValue.Ace},
+                        new Card(){Suit = CardSuit.Hearts, Value = CardValue.Ace},
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.Ace},
+                        new Card(){Suit = CardSuit.Diamonds, Value = CardValue.Five},
+                        new Card(){Suit = CardSuit.Clubs, Value = CardValue.Jack}
+                    };
+
+                // Act
+                var actual = target.Handle(hand);
+
+                // Assert
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
