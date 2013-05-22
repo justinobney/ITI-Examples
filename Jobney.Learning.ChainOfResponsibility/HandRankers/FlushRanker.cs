02dee15549705cb0d118c6796f511889544d95cf
Justin Obney <justin@envoc.com>
2013-05-22 07:54:49 -0500

    Coded the flush ranker

---
+++ Jobney.Learning.ChainOfResponsibility/HandRankers/FlushRanker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Jobney.Learning.ChainOfResponsibility.Enums;
+
+namespace Jobney.Learning.ChainOfResponsibility.HandRankers
+{
+    public class FlushRanker : HandRanker
+    {
+        public FlushRanker(HandRanker nextRanker) : base(nextRanker)
+        {
+            
+        }
+        public override HandRanking Handle(IEnumerable<Card> hand)
+        {
+            if (hand.GroupBy(x => x.Suit.Value).Any(grp => grp.Count() == 5))
+            {
+                CanHandle = true;
+                return HandRanking.Flush;
+            }
+            return next.Handle(hand);
+        }
+    }
+}
---
+++ Jobney.Learning.ChainOfResponsibility/Tests/FlushRankerTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jobney.Learning.ChainOfResponsibility.Enums;
+using Jobney.Learning.ChainOfResponsibility.HandRankers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jobney.Learning.ChainOfResponsibility.Tests
+{
+    [TestClass]
+    public class FlushRankerTest
+    {
+        public FlushRanker target;
+        public HighCardRanker highcardRanker;
+        private IEnumerable<Card> hand;
+
+        [TestInitialize]
+        public void Init()
+        {
+            highcardRanker = new HighCardRanker();
+            target = new FlushRanker(highcardRanker);
+        }
+
+        [TestClass]
+        public class TheHandleMethod : FlushRankerTest
+        {
+            [TestMethod]
+            public void ReturnsFlushRankWhenHandContainsFlush()
+            {
+                // Arrange
+                var expected = HandRanking.Flush;
+                hand = new List<Card>()
+                    {
+                        new Card(){Suit = CardSuit.Clubs, Value = CardValue.Nine},
+                        new Card(){Suit = CardSuit.Clubs, Value = CardValue.Six},
+                        new Card(){Suit = CardSuit.Clubs, Value = CardValue.Eight},
+                        new Card(){Suit = CardSuit.Clubs, Value = CardValue.Ace},
+                        new Card(){Suit = CardSuit.Clubs, Value = CardValue.Jack}
+                    };
+
+                // Act
+                var actual = target.Handle(hand);
+
+                // Assert
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestMethod]
+            public void ReturnsHighCardRankWhenNoFlushExists()
+            {
+                // Arrange
+                var expected = HandRanking.HighCard;
+                hand = new List<Card>()
+                    {
+                        new Card(){Suit = CardSuit.Clubs, Value = CardValue.Queen},
+                        new Card(){Suit = CardSuit.Hearts, Value = CardValue.Ace},
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.Eight},
+                        new Card(){Suit = CardSuit.Diamonds, Value = CardValue.Five},
+                        new Card(){Suit = CardSuit.Clubs, Value = CardValue.Jack}
+                    };
+
+                // Act
+                var actual = target.Handle(hand);
+
+                // Assert
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
