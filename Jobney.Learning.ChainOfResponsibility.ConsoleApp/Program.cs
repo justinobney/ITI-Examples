f7518a0e813082a366e0903c21aefb9d0653361e
Justin Obney <justinobney@gmail.com>
2013-05-23 00:30:42 -0500

    Changed program test code to run statistics of number of times a ranking is hit

--- Jobney.Learning.ChainOfResponsibility.ConsoleApp/Program.cs
+++ Jobney.Learning.ChainOfResponsibility.ConsoleApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq;
+using Jobney.Learning.ChainOfResponsibility.Enums;
+using Jobney.Learning.ChainOfResponsibility.HandRankers;
 
 namespace Jobney.Learning.ChainOfResponsibility.ConsoleApp
 {
@@ -7,21 +9,69 @@ namespace Jobney.Learning.ChainOfResponsibility.ConsoleApp
     {
         private static void Main(string[] args)
         {
-            var deck = new Deck();
+            Deck deck;
+            var pokerRanker = new PokerGameRanker();
 
-            for (int i = 0; i < 10; i++)
+            var highCardCounter = 0;
+            var pairCounter = 0;
+            var threeOfAKindCounter = 0;
+            var straightCounter = 0;
+            var flushCounter = 0;
+            var fullHouseCounter = 0;
+            var fourOfAKindCounter = 0;
+            var straightFlushCounter = 0;
+            var royalFlushCounter = 0;
+
+
+            for (int i = 0; i < 10000; i++)
             {
-                var hand = deck.Deal(5);
-                Console.WriteLine("============ Hand {0} ============", i);
-                foreach (var card in hand)
+                deck = new Deck();
+                for (int j = 0; j < 3; j++)
                 {
-                    Console.WriteLine("Card :: {0} of {1}", card.Value, card.Suit);
-                }
+                    var hand = deck.Deal(5);
 
-                Console.WriteLine("Cards Remaining: {0}", deck.Cards.Count());
-                Console.WriteLine("");
+                    switch (pokerRanker.Rank(hand))
+                    {
+                            case HandRanking.HighCard:
+                                highCardCounter++;
+                                break;
+                            case HandRanking.Pair:
+                                pairCounter++;
+                                break;
+                            case HandRanking.ThreeOfAKind:
+                                threeOfAKindCounter++;
+                                break;
+                            case HandRanking.Straight:
+                                straightCounter++;
+                                break;
+                            case HandRanking.Flush:
+                                flushCounter++;
+                                break;
+                            case HandRanking.FullHouse:
+                                fullHouseCounter++;
+                                break;
+                            case HandRanking.FourOfAKind:
+                                fourOfAKindCounter++;
+                                break;
+                            case HandRanking.StraightFlush:
+                                straightFlushCounter++;
+                                break;
+                            case HandRanking.RoyalFlush:
+                                royalFlushCounter++;
+                                break;
+                    }
+                }
             }
 
+            Console.WriteLine("High Card        ::: {0}", highCardCounter);
+            Console.WriteLine("Pair             ::: {0}", pairCounter);
+            Console.WriteLine("Three of a Kind  ::: {0}", threeOfAKindCounter);
+            Console.WriteLine("Straight         ::: {0}", straightCounter);
+            Console.WriteLine("Flush            ::: {0}", flushCounter);
+            Console.WriteLine("Full House       ::: {0}", fullHouseCounter);
+            Console.WriteLine("Four of a Kind   ::: {0}", fourOfAKindCounter);
+            Console.WriteLine("Straight Flush   ::: {0}", straightFlushCounter);
+            Console.WriteLine("Royal Flush      ::: {0}", royalFlushCounter);
             Console.ReadLine();
         }
     }
