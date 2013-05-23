45ea93e775df04d8cde154d45f2b69175c856727
Justin Obney <justinobney@gmail.com>
2013-05-22 22:57:18 -0500

    Added PokerGameRanker with tests.. (hint: it is incomplete in it's rankings)

---
+++ Jobney.Learning.ChainOfResponsibility/HandRankers/PokerGameRanker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Jobney.Learning.ChainOfResponsibility.Enums;
+
+namespace Jobney.Learning.ChainOfResponsibility.HandRankers
+{
+    public class PokerGameRanker
+    {
+        private readonly HighCardRanker highCardRanker;
+        private readonly PairRanker pairRanker;
+        readonly ThreeOfAKindRanker threeOfAKindRanker;
+        private readonly StraightRanker straightRanker;
+        private readonly FlushRanker flushRanker;
+        private readonly FullHouseRanker fullHouseRanker;
+        private readonly FourOfAKindRanker fourOfAKindRanker;
+        private readonly StraightFlushRanker straightFlushRanker;
+        private readonly RoyalFlushRanker royalFlushRanker;
+
+        public PokerGameRanker()
+        {
+            highCardRanker = new HighCardRanker();
+            pairRanker = new PairRanker(highCardRanker);
+            threeOfAKindRanker = new ThreeOfAKindRanker(pairRanker);
+            straightRanker = new StraightRanker(threeOfAKindRanker);
+            flushRanker = new FlushRanker(straightRanker);
+            fullHouseRanker = new FullHouseRanker(flushRanker);
+            fourOfAKindRanker = new FourOfAKindRanker(fullHouseRanker);
+            straightFlushRanker = new StraightFlushRanker(fourOfAKindRanker);
+            royalFlushRanker = new RoyalFlushRanker(straightFlushRanker);
+        }
+
+        public HandRanking Rank(IEnumerable<Card> hand)
+        {
+            return royalFlushRanker.Handle(hand);
+        }
+    }
+}
---
+++ Jobney.Learning.ChainOfResponsibility/Tests/GameRankerTests/PokerGameRankerTests.cs
@@ -0,0 +1,213 @@
+﻿using System.Collections.Generic;
+using Jobney.Learning.ChainOfResponsibility.Enums;
+using Jobney.Learning.ChainOfResponsibility.HandRankers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jobney.Learning.ChainOfResponsibility.Tests.GameRankerTests
+{
+    [TestClass]
+    public class PokerGameRankerTests
+    {
+        protected PokerGameRanker target;
+        protected IEnumerable<Card> hand;
+
+        [TestInitialize]
+        public void Init()
+        {
+            target = new PokerGameRanker();
+        }
+
+        [TestClass]
+        public class TheRankHandMethod : PokerGameRankerTests
+        {
+            [TestMethod]
+            public void ReturnsRoyalFlushWhenGivenARoyalFlush()
+            {
+                // Arrange
+                const HandRanking expected = HandRanking.RoyalFlush;
+                hand = new List<Card>()
+                    {
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.Ace},
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.King},
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.Queen},
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.Jack},
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.Ten}
+                    };
+
+                // Act
+                var actual = target.Rank(hand);
+
+                // Assert
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestMethod]
+            public void ReturnsStraightFlushWhenGivenAStraightFlush()
+            {
+                // Arrange
+                const HandRanking expected = HandRanking.StraightFlush;
+                hand = new List<Card>()
+                    {
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.Nine},
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.King},
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.Queen},
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.Jack},
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.Ten}
+                    };
+
+                // Act
+                var actual = target.Rank(hand);
+
+                // Assert
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestMethod]
+            public void ReturnsFourOfAKindWhenGivenAFourOfAKind()
+            {
+                // Arrange
+                const HandRanking expected = HandRanking.FourOfAKind;
+                hand = new List<Card>()
+                    {
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.Nine},
+                        new Card(){Suit = CardSuit.Diamonds, Value = CardValue.Nine},
+                        new Card(){Suit = CardSuit.Clubs, Value = CardValue.Nine},
+                        new Card(){Suit = CardSuit.Hearts, Value = CardValue.Nine},
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.Ten}
+                    };
+
+                // Act
+                var actual = target.Rank(hand);
+
+                // Assert
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestMethod]
+            public void ReturnsFullHouseWhenGivenAFullHouse()
+            {
+                // Arrange
+                const HandRanking expected = HandRanking.FullHouse;
+                hand = new List<Card>()
+                    {
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.Nine},
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.Seven},
+                        new Card(){Suit = CardSuit.Clubs, Value = CardValue.Nine},
+                        new Card(){Suit = CardSuit.Hearts, Value = CardValue.Nine},
+                        new Card(){Suit = CardSuit.Diamonds, Value = CardValue.Seven}
+                    };
+
+                // Act
+                var actual = target.Rank(hand);
+
+                // Assert
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestMethod]
+            public void ReturnsFlushWhenGivenAFlush()
+            {
+                // Arrange
+                const HandRanking expected = HandRanking.Flush;
+                hand = new List<Card>()
+                    {
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.King},
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.Jack},
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.Four},
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.Nine},
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.Seven}
+                    };
+
+                // Act
+                var actual = target.Rank(hand);
+
+                // Assert
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestMethod]
+            public void ReturnsStraightWhenGivenAStraight()
+            {
+                // Arrange
+                const HandRanking expected = HandRanking.Straight;
+                hand = new List<Card>()
+                    {
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.Nine},
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.Seven},
+                        new Card(){Suit = CardSuit.Clubs, Value = CardValue.Eight},
+                        new Card(){Suit = CardSuit.Hearts, Value = CardValue.Six},
+                        new Card(){Suit = CardSuit.Diamonds, Value = CardValue.Five}
+                    };
+
+                // Act
+                var actual = target.Rank(hand);
+
+                // Assert
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestMethod]
+            public void ReturnsThreeOfAKindWhenGivenAThreeOfAKind()
+            {
+                // Arrange
+                const HandRanking expected = HandRanking.ThreeOfAKind;
+                hand = new List<Card>()
+                    {
+                        new Card(){Suit = CardSuit.Clubs, Value = CardValue.Three},
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.Three},
+                        new Card(){Suit = CardSuit.Clubs, Value = CardValue.Five},
+                        new Card(){Suit = CardSuit.Hearts, Value = CardValue.Three},
+                        new Card(){Suit = CardSuit.Diamonds, Value = CardValue.Six}
+                    };
+
+                // Act
+                var actual = target.Rank(hand);
+
+                // Assert
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestMethod]
+            public void ReturnsPairWhenGivenAPair()
+            {
+                // Arrange
+                const HandRanking expected = HandRanking.Pair;
+                hand = new List<Card>()
+                    {
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.Nine},
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.Seven},
+                        new Card(){Suit = CardSuit.Clubs, Value = CardValue.Nine},
+                        new Card(){Suit = CardSuit.Hearts, Value = CardValue.Jack},
+                        new Card(){Suit = CardSuit.Diamonds, Value = CardValue.Seven}
+                    };
+
+                // Act
+                var actual = target.Rank(hand);
+
+                // Assert
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestMethod]
+            public void ReturnsHighCardWhenGivenAHighCard()
+            {
+                // Arrange
+                const HandRanking expected = HandRanking.HighCard;
+                hand = new List<Card>()
+                    {
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.Nine},
+                        new Card(){Suit = CardSuit.Spades, Value = CardValue.Seven},
+                        new Card(){Suit = CardSuit.Clubs, Value = CardValue.Jack},
+                        new Card(){Suit = CardSuit.Hearts, Value = CardValue.Queen},
+                        new Card(){Suit = CardSuit.Diamonds, Value = CardValue.Two}
+                    };
+
+                // Act
+                var actual = target.Rank(hand);
+
+                // Assert
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
